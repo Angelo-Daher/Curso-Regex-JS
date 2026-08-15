0bf04d6f945d329c3e857a157115a6b6a1f14dc5
Ângelo Daher <angelogabrielangelo890@gmail.com>
2026-08-15 16:41:02 -0300

    Alguns Cuidados - Cuidados com E.R

---
+++ caracteres/AlgunsCuidados.js
@@ -0,0 +1,12 @@
+const textoUmaLinha = '...' // aspas simples ou duplas
+
+const textoMultiLinhas = ` 
+linha 1
+linha 2
+` // texto com múltiplas linhas em js é com crase ``!
+
+// cuidado com o Tab!
+console.log('   '.match(/\s/g))
+
+// cuidados na hora de executar o código 
+// se vc selecionar (marcar) uma linha somente e executar, ele não vai executar o coódigo completo, mas sim somente aquela linha que vc selecionou!
