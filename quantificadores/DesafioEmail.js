6e9a0386313230ae775c785e64fd4ab85c29b265
Ângelo Daher <angelogabrielangelo890@gmail.com>
2026-08-17 11:58:12 -0300

    Desafio Emails

--- quantificadores/DesafioEmail.js
+++ quantificadores/DesafioEmail.js
@@ -3,14 +3,14 @@ Os e-mails dos convidados são:
     - fulãno@cod3r.com.br
     - xicó@gmail.com
     - joÂo@empresa.info.br
-    - mariá_silva@registro.br
+    - mariá_silva@régistro.br
     - rafa.sampaio@yahoo.com
 `
 
 console.log(texto.match(/[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.[a-zA-Z0-9_]{2,4}/g)) // de forma sem usar as shorthands
 console.log(texto.match(/.+@\w+\.\w{2,4}\.*\w{0,2}/gi)) // Contando com caracteres especiais (acentuação)
-console.log(texto.match(/[\w.]+@\w+\.\w{2,4}\.?\w{0,2}/g)) // sem contar com caracteres especiais (acentuação)
+console.log(texto.match(/(\w.[Á-ú]|[A-z])+@\w+\.\w{2,4}\.?\w{0,2}/g)) // sem contar com caracteres especiais (acentuação)
 
 // no futuro...
 
-console.log(texto.match(/[\w+]+@\w+\.\w{2,4}(\.\w{2})?/g)) // Forma de grupo para deixar mais organizado!
+console.log(texto.match(/(\w.[Á-ú]|[A-z])+@\w.+\.\w{2,4}(\.\w{2})?/g)) // Forma de grupo para deixar mais organizado!
